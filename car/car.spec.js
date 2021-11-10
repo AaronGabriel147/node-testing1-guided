3c9517c31186c85f5839417580b9c85e4d6d3c28
Ladrillo <gabriel@cabrejas.net>
2021-11-10 19:18:57 +0100

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -58,6 +58,6 @@ describe('Car class', () => {
     expect(prius.model).toBe('prius')
   })
   it('all new cars have an odometer set at 0 miles', () => {
-    expect(prius).toMatchObject({ odometer})
+    expect(prius).toMatchObject({ odometer: 0 })
   })
 })
