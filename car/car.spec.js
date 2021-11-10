f1032cd5b875b3a26ab184c34781dafa39804489
Ladrillo <gabriel@cabrejas.net>
2021-11-10 18:26:39 +0100

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -1,1 +1,13 @@
-// Test away!
+describe('Intro to testing', () => {
+  test('sanity', () => { // test
+    // assertions are usually closely related
+    expect(true).toBe(true) // assertion
+    expect(true).not.toBe(false) // another assertion
+  })
+
+  it('works', () => {
+    const expectedValue = 8
+    const actualValue = 8 // this usually comes from the function we're testing
+    expect(actualValue).toBe(expectedValue)
+  })
+})
